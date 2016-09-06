189f8d252d0a6180f8174002ef5757288f17d347
Eduardo Fernández <e.fernandez@sh-060-mc.sherpa.local>
2016-09-06 18:17:32 +0200

    refactored all static files to be in the thin client layer

---
+++ service-boot-lib/src/main/java/com/pigdroid/springangularjs/business/service/criteria/CitySearchCriteria.java
@@ -0,0 +1,96 @@
+package com.pigdroid.springangularjs.business.service.criteria;
+
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.jpa.domain.Specification;
+
+import com.pigdroid.springangularjs.business.entity.CityEntity;
+import com.pigdroid.springangularjs.business.entity.CityEntity_;
+import com.pigdroid.springangularjs.utils.JPAPatterns;
+
+public class CitySearchCriteria implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String name;
+	private String country;
+	private String orderBy;
+	private String orderMode;
+
+
+	public CitySearchCriteria() {
+	}
+
+	public CitySearchCriteria(String name) {
+//		Assert.notNull(name, "Name must not be null");
+		this.name = name;
+	}
+
+	public String getOrderBy() {
+		return orderBy;
+	}
+
+	public void setOrderBy(String orderBy) {
+		this.orderBy = orderBy;
+	}
+
+	public String getOrderMode() {
+		return orderMode;
+	}
+
+	public void setOrderMode(String orderMode) {
+		this.orderMode = orderMode;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+
+	public static Specification<CityEntity> search(final CitySearchCriteria cri) {
+		return new Specification<CityEntity>() {
+
+			public Predicate toPredicate(Root<CityEntity> root, CriteriaQuery<?> criteriaQuery,
+					CriteriaBuilder criteriaBuilder) {
+
+				List<Predicate> predicates = new ArrayList<Predicate>();
+
+				if( StringUtils.isNotBlank(cri.getName()) ){
+					predicates.add( criteriaBuilder.like(root.get(CityEntity_.name),
+							JPAPatterns.getContainsLikePattern(cri.getName())) );
+				}
+				// builder.between(pRoot.get(Person_.time), startDate,
+				// endDate)
+				return criteriaBuilder.and(predicates.toArray(new Predicate[predicates.size()]));
+			}
+		};
+	}
+
+	public void set(HttpServletRequest req) {
+		this.name = (String) req.getParameter("name");
+		this.orderBy = (String) req.getParameter("orderBy");
+		this.orderMode = (String) req.getParameter("orderMode");
+	}
+}
---
+++ service-boot-lib/src/main/java/com/pigdroid/springangularjs/web/controller/SampleController.java
@@ -0,0 +1,134 @@
+package com.pigdroid.springangularjs.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.ExposesResourceFor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pigdroid.springangularjs.business.entity.CityEntity;
+import com.pigdroid.springangularjs.business.service.CityService;
+import com.pigdroid.springangularjs.business.service.CrudService;
+import com.pigdroid.springangularjs.business.service.criteria.CitySearchCriteria;
+import com.pigdroid.springangularjs.utils.PageWrapper;
+
+@RequestMapping("/city")
+@Controller
+@ExposesResourceFor(CityEntity.class)
+public class SampleController extends CrudController<CityEntity> {
+
+	@Autowired
+	private CityService cityService;
+
+	public CrudService<CityEntity> getService(){
+		return cityService;
+	}
+
+	public CityEntity getInstance(){
+		return new CityEntity();
+	}
+
+	public String getItemName(){
+		return "city";
+	}
+	public String getItemsName(){
+		return "cities";
+	}
+	public String getOrderByDefault(){
+		return "name";
+	}
+	public String getOrderModeDefault(){
+		return "asc";
+	}
+
+
+    @Override
+    public String listItems(Model uiModel, Pageable page, HttpServletRequest request) {
+        PageWrapper<CityEntity> pageNew = new PageWrapper<CityEntity> (cityService.findCities(new CitySearchCriteria(), page), "/city/list");
+        uiModel.addAttribute("page", pageNew);
+        uiModel.addAttribute("items", pageNew.getContent());
+        uiModel.addAttribute("controller", "city");
+        uiModel.addAttribute("criteria", new CitySearchCriteria());
+
+        makeMessage(uiModel, request);
+        return "city/cities";
+    }
+
+	@Override
+	public String listItemsFilter(Model uiModel, Pageable page, HttpServletRequest request) {
+    	CitySearchCriteria criteria = new CitySearchCriteria();
+    	criteria.set(request);
+    	String direction = getOrderModeDefault();
+    	if(StringUtils.isNotBlank( criteria.getOrderMode() ))
+    		direction = criteria.getOrderMode();
+
+    	String fieldName = getOrderByDefault();
+    	if(StringUtils.isNotBlank( criteria.getOrderBy() ))
+    		fieldName = criteria.getOrderBy();
+
+    	page = createPageRequest(page, direction, fieldName);
+        PageWrapper<CityEntity> pageNew = new PageWrapper<CityEntity> (cityService.findCities(criteria, page), "/city/list");
+        uiModel.addAttribute("page", pageNew);
+        uiModel.addAttribute("items", pageNew.getContent());
+        uiModel.addAttribute("controller", "city");
+        uiModel.addAttribute("criteria", criteria);
+
+        makeMessage(uiModel, request);
+        return "city/cities";
+	}
+
+
+
+//    @RequestMapping("city/{id}")
+//    public String showITem(@PathVariable Long id, Model model){
+//        model.addAttribute("city", cityService.getItemById(id));
+//    	model.addAttribute("mode", "readonly");
+//        model.addAttribute("controller", "city");
+//        return "city/detail";
+//    }
+//
+//    @RequestMapping("city")
+//    public String showItem(Model model){
+//    	model.addAttribute("city", new CityEntity());
+//    	model.addAttribute("mode", "readonly");
+//        model.addAttribute("controller", "city");
+//        return "city/detail";
+//    }
+//
+//
+//
+//    @RequestMapping("city/edit/{id}")
+//    public String editItem(@PathVariable Long id, Model model){
+//        model.addAttribute("city", cityService.getItemById(id));
+//    	model.addAttribute("mode", "edit");
+//        model.addAttribute("controller", "city");
+//        return "city/detail";
+//    }
+//
+//    @RequestMapping("city/new")
+//    public String newItem(Model model){
+//        model.addAttribute("city", new CityEntity());
+//    	model.addAttribute("mode", "new");
+//        model.addAttribute("controller", "city");
+//    	return "city/detail";
+//    }
+//
+//    @RequestMapping(value = "city", method = RequestMethod.POST)
+//    public String saveItem(CityEntity item){
+//        cityService.addItem(item);
+//        return "redirect:/cities";
+//    }
+//
+//    @RequestMapping("city/delete/{id}")
+//    public String deleteItem(@PathVariable Long id){
+//        cityService.deleteItem(id);
+//        return "redirect:/cities?successMessage=message.remove.correct";
+//    }
+
+
+
+}
